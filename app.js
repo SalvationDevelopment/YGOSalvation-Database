8538152d12e3968a4105df935d24ba7204416ca0
Jamezs Gladney <panthrowzay@gmail.com>
2017-05-23 01:41:58 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
 /*jslint node:true*/
 'use strict';
 
+process.shell = require('shelljs'); // https://github.com/shelljs/shelljs
+
+var enviroment = require('dotenv').config(),
+	database = require('./connection.js');
+
+
+Object.keys(enviroment.parsed).forEach(function (env) {
+	if (enviroment.parsed[env] !== process.env[env]) {
+		console.log('Enviromental Variable "' + env + '" is not in the `.env` file.');
+	}
+});
+
+process.database = database(function (error, database) {
+	if (error) {
+		console.log(error.message);
+	}
+
+	console.log('Connected to:', process.env.VOICE_DB_URL);
+});
+
 var express = require('express'),
 	path = require('path'),
 	helmet = require('helmet'),
