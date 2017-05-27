af76eb3831c959fc6ea38da684e1d44f83747398
Amine Belaid <AmineBelaid2909@gmail.com>
2017-05-26 22:17:07 -0400

    Changes

--- http/JSONSplitter.js
+++ http/JSONSplitter.js
@@ -1,12 +1,16 @@
 //JSONSplitter.py
 //By: HelixReactor
-//Last Modified: 26/05/2017, 21:49 (GMT -4)
+//Last Modified: 26/05/2017, 22:16 (GMT -4)
 
-var full = require("./manifest_0-en-OCGTCG.json");
+function split_json() {
+    var full = require("./manifest_0-en-OCGTCG.json");
 
-full.forEach(function(card){
-    var data = JSON.stringify(card),
+    full.forEach(function(card){
+        var data = JSON.stringify(card),
         id = "./json/" + (card.id).toString() + ".json";
     
-    fs.writeFileSync(id, data);
-});
+        fs.writeFileSync(id, data);
+    });
+}
+
+module.exports = split_json();
