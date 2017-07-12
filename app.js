bbd5e5eeb951e3bf59abc6a4d95b40f12378b213
Zayelion <panthrowzay@gmail.com>
2017-07-12 01:05:18 -0400

    testing and new browser

--- app.js
+++ app.js
@@ -8,29 +8,30 @@ process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
 var enviroment = require('dotenv').config();
 
 if (enviroment.parsed) {
-	Object.keys(enviroment.parsed).forEach(function (env) {
-		if (enviroment.parsed[env] !== process.env[env]) {
-			console.log('Enviromental Variable "' + env + '" is not in the `.env` file.');
-		}
-	});
+    Object.keys(enviroment.parsed).forEach(function(env) {
+        if (enviroment.parsed[env] !== process.env[env]) {
+            console.log('Enviromental Variable "' + env + '" is not in the `.env` file.');
+        }
+    });
 }
 
 const fs = require('fs'),
-	http = require('https'),
-	express = require('express'),
-	path = require('path'),
-	helmet = require('helmet'),
-	passport = require('passport'),
-	bodyParser = require('body-parser'),
-	app = express(),
-	child_process = require('child_process'),
-	jsonGenerator = require('./generator.js'),
-	server = require('http').createServer(app),
-	Primus = require('primus'),
-	primus = new Primus(server, {
-		transformer: 'websockets',
-		parser: 'JSON'
-	});
+    http = require('https'),
+    express = require('express'),
+    path = require('path'),
+    helmet = require('helmet'),
+    passport = require('passport'),
+    bodyParser = require('body-parser'),
+    app = express(),
+    child_process = require('child_process'),
+    jsonGenerator = require('./generator.js'),
+    serveIndex = require('serve-index'),
+    server = require('http').createServer(app),
+    Primus = require('primus'),
+    primus = new Primus(server, {
+        transformer: 'websockets',
+        parser: 'JSON'
+    });
 
 process.env.LOCALHOST_PORT = process.env.API_LOCALHOST_PORT || 8082;
 
@@ -39,7 +40,7 @@ process.env.LOCALHOST_PORT = process.env.API_LOCALHOST_PORT || 8082;
 // Connect the middleware to express.
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-	extended: false
+    extended: false
 }));
 app.use(helmet());
 app.use(passport.initialize());
@@ -48,6 +49,7 @@ primus.save(__dirname + '/http/js/vendor/primus.js');
 console.log(path.join(__dirname, '/http'));
 // Connect Express Routes
 app.use(express['static'](path.join(__dirname, '/http')));
+app.use('/', serveIndex((path.join(__dirname, '/http'))));
 
 
 
@@ -57,10 +59,10 @@ server.listen(process.env.LOCALHOST_PORT);
 console.log('Application can be found at http://127.0.0.1:' + process.env.LOCALHOST_PORT);
 
 // Setup websocket connection for doing stuff.
-primus.on('connection', function (spark) {
-	spark.write({
-		message: 'hello'
-	});
+primus.on('connection', function(spark) {
+    spark.write({
+        message: 'hello'
+    });
 });
 
 /**
@@ -71,43 +73,43 @@ primus.on('connection', function (spark) {
  */
 function regenerate(request, response, next) {
 
-	jsonGenerator.getDB(response, function (error, newJSON) {
-		const mainifest = JSON.stringify(newJSON);
-		response.write('Saving!\r\n');
-		fs.writeFile('./http/manifest.json', mainifest, function () {
-			response.write('Saved, Notifying...\r\n');
-			var call = http.get({
-				host: 'ygopro.us',
-				path: '/git'
-			}, function (appresponse) {
-				response.write('Notified!\r\n');
-				response.end();
-				next();
-			});
-			call.on('data', function (data) {
-				response.write(data);
-			});
-			call.on('error', function (errorMessage) {
-				response.write('Unable to notify main server\r\n');
-				response.write(JSON.stringify(errorMessage));
-				response.end();
-				next();
-			});
-			call.end();
-		});
-	});
+    jsonGenerator.getDB(response, function(error, newJSON) {
+        const mainifest = JSON.stringify(newJSON);
+        response.write('Saving!\r\n');
+        fs.writeFile('./http/manifest.json', mainifest, function() {
+            response.write('Saved, Notifying...\r\n');
+            var call = http.get({
+                host: 'ygopro.us',
+                path: '/git'
+            }, function(appresponse) {
+                response.write('Notified!\r\n');
+                response.end();
+                next();
+            });
+            call.on('data', function(data) {
+                response.write(data);
+            });
+            call.on('error', function(errorMessage) {
+                response.write('Unable to notify main server\r\n');
+                response.write(JSON.stringify(errorMessage));
+                response.end();
+                next();
+            });
+            call.end();
+        });
+    });
 }
 
 function gitRoute(request, response, next) {
-	response.setHeader('Content-Type', 'application/json');
-	response.write('Attempting to Update Server...\r\n');
-	var gitShell = child_process.exec('git pull', {}, function (error, stdout, stderr) {
-		if (error) {
-			response.write(JSON.stringify(error) + '\r\n\r\n');
-		}
-		response.write(JSON.stringify(stdout) + '\r\n\r\n');
-		regenerate(request, response, next);
-	});
+    response.setHeader('Content-Type', 'application/json');
+    response.write('Attempting to Update Server...\r\n');
+    var gitShell = child_process.exec('git pull', {}, function(error, stdout, stderr) {
+        if (error) {
+            response.write(JSON.stringify(error) + '\r\n\r\n');
+        }
+        response.write(JSON.stringify(stdout) + '\r\n\r\n');
+        regenerate(request, response, next);
+    });
 }
 
 // Setup routes
--- test/test.js
+++ test/test.js
@@ -2,25 +2,35 @@ var walk = require('fs-walk'),
     fs = require('fs'),
     assert = require('chai').assert;
 
-describe('Card form', function () {
-    walk.filesSync('./http/json/', function (basedir, filename, stat, next) {
+describe('Card form', function() {
+    walk.filesSync('./http/json/', function(basedir, filename, stat, next) {
         var data = fs.readFileSync(basedir + filename);
-        describe(filename + ' should be parsable', function (done) {
+        describe(filename + ' should be parsable', function(done) {
             var card = JSON.parse(data);
-            it('should be have a name', function (done) {
+            it('should be have a proper formatting', function(done) {
                 assert.isString(card.name);
-                done();
-            });
-            it('should be have a desc', function (done) {
                 assert.isString(card.desc);
-                done();
-            });
-            it('should be have an id', function (done) {
                 assert.isNumber(card.id);
-                done();
-            });
-            it('should be have a type', function (done) {
                 assert.isNumber(card.type);
+                assert.isObject(card.tcg);
+                if (card.tcg.id) {
+                    assert.isOk(card.tcg.pack);
+                    assert.isOk(card.tcg.pack_id);
+                }
+                assert.isObject(card.ocg);
+                if (card.ocg.id) {
+                    assert.isOk(card.ocg.pack);
+                    assert.isOk(card.ocg.pack_id);
+                }
+                if (card.def !== undefined) {
+                    assert.isNumber(card.def);
+                }
+                if (card.atk !== undefined) {
+                    assert.isNumber(card.atk);
+                }
+                if (card.level !== undefined) {
+                    assert.isNumber(card.level);
+                }
                 done();
             });
         });
